2026b369e0c3895fc8bf7a9b09855a98b8279281
Shagun-08 <108570475+Shagun-08@users.noreply.github.com>
2023-07-27 14:41:31 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger")
-const navlist = document.getElementById("navlist")
-
-hamburger.addEventListener("click", ()=>{
-
-    navlist.classList.toggle("navlist-active")
-})
